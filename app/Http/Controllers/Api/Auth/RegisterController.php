78a7c6f9d01f6da6cb4b687ec1a5c933c69901d0
Najmul Hossain <phpnajmul@gmail.com>
2024-03-21 19:06:59 +0600

    Laravel API Auth Polash Mahmud Follow

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        $user = User::create([
+           'name' => $request->input('name'),
+           'email' => $request->input('email'),
+           'password' => bcrypt($request->input('password')),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ], 201);
+
+    }
+
+
+}
